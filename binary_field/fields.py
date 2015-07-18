289d7df82b43b61f0ddee5cbec6ddc5fafc3f7b6
Sébastien BEAU <sebastien.beau@akretion.com>
2015-07-18 14:39:27 +0200

    [REF] start porting and refactoring to 8, remove useless code and move some code into storage directory

--- binary_field/fields.py
+++ binary_field/fields.py
@@ -20,127 +20,11 @@
 #
 ###############################################################################
 
-import hashlib
-from openerp.osv import fields, orm
+from openerp import fields, models
 from openerp.tools import image_resize_image
 from openerp.tools.translate import _
 from openerp import tools
-import os
-import sys
-import logging
 
-_logger = logging.getLogger(__name__)
-
-from openerp import SUPERUSER_ID
-
-
-class Storage(object):
-
-    def __init__(self, cr, uid, model_name, field_name, record, config):
-        self.cr = cr
-        self.uid = uid
-        self.pool = record._model.pool
-        self.field_name = field_name
-        self.model_name = model_name
-        self.config = config
-        self.external_storage_server = config['external_storage_server']
-
-
-class FileSystemStorage(Storage):
-
-    def _full_path(self, cr, uid, fname):
-        return os.path.join(
-            self.config['base_path'],
-            self.cr.dbname,
-            '%s-%s' % (self.model_name, self.field_name),
-            fname)
-
-    # Code extracted from Odoo V8 in ir_attachment.py
-    # Copyright (C) 2004-2014 OPENERP SA
-    # Licence AGPL V3
-    def _get_path(self, cr, uid, bin_data):
-        sha = hashlib.sha1(bin_data).hexdigest()
-        # scatter files across 256 dirs
-        # we use '/' in the db (even on windows)
-        fname = sha[:2] + '/' + sha
-        full_path = self._full_path(cr, uid, fname)
-        dirname = os.path.dirname(full_path)
-        if not os.path.isdir(dirname):
-            os.makedirs(dirname)
-        return fname, full_path
-
-    def _file_read(self, cr, uid, fname, bin_size=False):
-        full_path = self._full_path(cr, uid, fname)
-        r = ''
-        try:
-            if bin_size:
-                r = os.path.getsize(full_path)
-            else:
-                r = open(full_path,'rb').read().encode('base64')
-        except IOError:
-            _logger.error("_read_file reading %s",full_path)
-        return r
-
-    def _file_write(self, cr, uid, value):
-        bin_value = value.decode('base64')
-        fname, full_path = self._get_path(cr, uid, bin_value)
-        if not os.path.exists(full_path):
-            try:
-                with open(full_path, 'wb') as fp:
-                    fp.write(bin_value)
-            except IOError:
-                _logger.error("_file_write writing %s", full_path)
-        return fname
-
-    def _file_delete(self, cr, uid, fname):
-        obj = self.pool[self.model_name]
-        count = obj.search(cr, 1, [
-            ('%s_uid' % self.field_name, '=', fname),
-            ], count=True)
-        full_path = self._full_path(cr, uid, fname)
-        if count <= 1 and os.path.exists(full_path):
-            try:
-                os.unlink(full_path)
-            except OSError:
-                _logger.error("_file_delete could not unlink %s",full_path)
-            except IOError:
-                # Harmless and needed for race conditions
-                _logger.error("_file_delete could not unlink %s",full_path)
-    # END of extraction
-
-    def add(self, value):
-        if not value:
-            return {}
-        file_size = sys.getsizeof(value.decode('base64'))
-        _logger.debug('Add binary to model: %s, field: %s'
-                      % (self.model_name, self.field_name))
-        binary_uid = self._file_write(self.cr, SUPERUSER_ID, value)
-        return {
-            'binary_uid': binary_uid,
-            'file_size': file_size,
-            }
-
-    def update(self, binary_uid, value):
-        _logger.debug('Delete binary model: %s, field: %s, uid: %s'
-                      % (self.model_name, self.field_name, binary_uid))
-        self._file_delete(self.cr, SUPERUSER_ID, binary_uid)
-        if not value:
-            return {}
-        return self.add(value)
-
-    def get(self, binary_uid):
-        if not binary_uid:
-            return None
-        return self._file_read(self.cr, SUPERUSER_ID, binary_uid)
-
-    def get_url(self, binary_uid):
-        if not binary_uid:
-            return None
-        return os.path.join(
-            self.config['base_external_url'],
-            self.cr.dbname,
-            '%s-%s' % (self.model_name, self.field_name),
-            binary_uid)
 
 
 class BinaryField(fields.function):
@@ -320,30 +204,3 @@ class ImageField(BinaryField):
 
 fields.BinaryField = BinaryField
 fields.ImageField = ImageField
-
-
-original__init__ = orm.BaseModel.__init__
-
-
-def __init__(self, pool, cr):
-    if pool.get('binary.field.installed'):
-        additional_field = {}
-        for field_name in self._columns:
-            field = self._columns[field_name]
-            if isinstance(field, BinaryField):
-                additional_field.update({
-                    '%s_uid' % field_name:
-                        fields.char('%s UID' % self._columns[field_name].string),
-                    '%s_file_size' % field_name:
-                        fields.integer(
-                            '%s File Size' % self._columns[field_name].string),
-                    })
-        self._columns.update(additional_field)
-    original__init__(self, pool, cr)
-
-
-orm.BaseModel.__init__ = __init__
-
-
-class BinaryFieldInstalled(orm.AbstractModel):
-    _name = 'binary.field.installed'
---
+++ binary_field/storage/abstract.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   Module for OpenERP
+#   Copyright (C) 2014 Akretion (http://www.akretion.com).
+#   @author Sébastien BEAU <sebastien.beau@akretion.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
+class Storage(object):
+
+    def __init__(self, cr, uid, model_name, field_name, record, config):
+        self.cr = cr
+        self.uid = uid
+        self.pool = record._model.pool
+        self.field_name = field_name
+        self.model_name = model_name
+        self.config = config
+        self.external_storage_server = config['external_storage_server']
