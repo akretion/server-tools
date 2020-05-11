2ffff5dca4dd9a96e69f00d2f27ddfb4d2138e6c
David Beal <david.beal@akretion.com>
2020-05-11 16:27:38 +0200

    FIX typo and readme

--- attachment_queue/__manifest__.py
+++ attachment_queue/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Attachment Queue",
     "version": "12.0.1.0.0",
     "author": "Akretion,Odoo Community Association (OCA)",
-    "summary": "Base module that add the concept of queue for processing file",
+    "summary": "Base module that adding the concept of queue for processing file",
     "website": "https://github.com/OCA/server-tools",
     "maintainers": ["florian-dacosta", "sebastienbeau"],
     "license": "AGPL-3",
