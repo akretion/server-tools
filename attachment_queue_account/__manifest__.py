38c6abc9a0229180a3b2f1836ed5d194ee3f643a
Florian da Costa <florian.dacosta@akretion.com>
2022-02-14 16:44:55 +0100

    [MIG] attachment_queue_account to v14

--- attachment_queue_account/__manifest__.py
+++ attachment_queue_account/__manifest__.py
@@ -1,21 +1,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-
 {
-    'name': 'Attachment Queue Account',
-    'version': '12.0.1.0.0',
-    'category': 'Generic Modules/Others',
-    'license': 'AGPL-3',
-    'description': """
-            Allow to import bank statement files or account move
-            files attachment queue
-        """,
-    'author': 'Akretion',
-    'website': 'http://www.akretion.com/',
-    'depends': [
-        'attachment_queue',
-        'account_move_base_import'], 
-    'data': [ 
-        'views/attachment_queue.xml',
+    "name": "Attachment Queue Account",
+    "version": "14.0.1.0.0",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainer": "Akretion",
+    "category": "Finance",
+    "complexity": "easy",
+    "depends": [
+        "account_move_base_import",
+        "account_statement_import",
+        "attachment_queue",
     ],
-    'installable': True,
+    "website": "https://github.com/OCA/server-tools",
+    "data": ["views/attachment_queue.xml"],
+    "installable": True,
+    "auto_install": False,
+    "license": "AGPL-3",
 }
--- attachment_queue_account/models/account_bank_statement.py
+++ attachment_queue_account/models/account_bank_statement.py
@@ -1,11 +1,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class AccountBankStatement(models.Model):
     _inherit = "account.bank.statement"
 
-    attachement_queue_id = fields.Many2one(
-        'attachment.queue'
-    )
+    attachement_queue_id = fields.Many2one("attachment.queue")
