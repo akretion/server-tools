4abf89191e1499928258f33dd8ff37750b0444b7
Florian da Costa <florian.dacosta@akretion.com>
2023-02-09 14:51:21 +0100

    [16.0][MIG] Migrate attachment_queue_account to 16

--- attachment_queue_account/__manifest__.py
+++ attachment_queue_account/__manifest__.py
@@ -1,14 +1,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     "name": "Attachment Queue Account",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainer": "Akretion",
     "category": "Finance",
     "complexity": "easy",
     "depends": [
         "account_move_base_import",
-        "account_statement_import",
+        "account_statement_import_file",
         "attachment_queue",
     ],
     "website": "https://github.com/OCA/server-tools",
