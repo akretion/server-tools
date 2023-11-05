fd7c6fbc7b23ab00c8aa659b65f056b1b6aafb3c
Sébastien BEAU <sebastien.beau@akretion.com>
2023-11-05 14:10:59 +0100

    onchange_helper: fix playing onchange

--- onchange_helper/models/base.py
+++ onchange_helper/models/base.py
@@ -51,7 +51,11 @@ class Base(models.AbstractModel):
         ]
         onchange_specs = {field_name: "1" for field_name, field in editable_fields}
         editable_fields_name = [x[0] for x in editable_fields]
-        onchange_fields = [field_name for field_name in editable_fields_name]
+        onchange_fields = [
+            field_name
+            for field_name in onchange_fields
+            if field_name in editable_fields_name
+        ]
         all_values = values.copy()
         # If self is a record (play onchange on existing record)
         # we take the value of the field
