e235ca440bd69eb0eef1d75780b70ba092cabbd2
Florian da Costa <florian.dacosta@akretion.com>
2025-05-12 15:52:01 +0200

    Backport attachment_queue_email from v16

--- attachment_queue_email/__manifest__.py
+++ attachment_queue_email/__manifest__.py
@@ -5,14 +5,14 @@
 
 {
     "name": "Attachment Queue Email",
-    "version": "16.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Akretion,Odoo Community Association (OCA)",
     "category": "Generic Modules",
     "summary": "Create attachment from emails to be processed depending on their type",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/server-tools",
     "maintainers": ["florian-dacosta", "sebastienbeau", "bealdav"],
-    "depends": ["attachment_queue", "mail"],
+    "depends": ["attachment_queue", "fetchmail"],
     "demo": [],
     "data": [
         "security/ir.model.access.csv",
