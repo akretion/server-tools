aa0eac3343bcdedb396d9463ff269bdeb3472e79
Florian da Costa <florian.dacosta@akretion.com>
2025-05-12 14:18:38 +0200

    [IMP] Rename attachment_queue_account to attachment_queue_account_move_import and remove statement import

--- attachment_queue_account_move_import/__manifest__.py
+++ attachment_queue_account_move_import/__manifest__.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
-    "name": "Attachment Queue Account",
+    "name": "Attachment Queue Account Move Import",
     "version": "14.0.1.0.0",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainer": "Akretion",
@@ -8,7 +8,6 @@
     "complexity": "easy",
     "depends": [
         "account_move_base_import",
-        "account_statement_import",
         "attachment_queue",
     ],
     "website": "https://github.com/OCA/server-tools",
--- attachment_queue_account_move_import/models/__init__.py
+++ attachment_queue_account_move_import/models/__init__.py
@@ -1,4 +1,3 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 from . import account_move
-from . import account_bank_statement
 from . import attachment_queue
